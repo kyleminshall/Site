3f43ee451d129fc449b60e3d6722ccca41a91c40
Kyle Minshall <kyleminshall@gmail.com>
2014-08-27 22:59:52 -0700

    UGH

--- xss/index.php
+++ xss/index.php
@@ -20,7 +20,40 @@
 	$info_rows = mysql_num_rows($info); 
 	
 	if($info_rows > 0) 
-	{ 
+	{
+?>
+		<script language="javascript">
+		function setCookie(cname, cvalue, exdays) {
+		    var d = new Date();
+		    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+		    var expires = "expires="+d.toUTCString();
+		    document.cookie = cname + "=" + cvalue + "; " + expires;
+		}
+
+		function getCookie(cname) {
+		    var name = cname + "=";
+		    var ca = document.cookie.split(';');
+		    for(var i=0; i<ca.length; i++) {
+		        var c = ca[i];
+		        while (c.charAt(0)==' ') c = c.substring(1);
+		        if (c.indexOf(name) != -1) return c.substring(name.length, c.length);
+		    }
+		    return "";
+		}
+
+		function checkCookie() {
+		    var user = getCookie("username");
+		    if (user != "") {
+		        alert("Welcome again " + user);
+		    } else {
+		        user = prompt("Please enter your name:", "");
+		        if (user != "" && user != null) {
+		            setCookie("username", user, 365);
+		        }
+		    }
+		}</script>
+<?php
+		echo '<script language="javascript">document.write("<b>You have been to this page "+gettimes()+" before.</b>");</script>';
 		echo '<script language="javascript">document.write("<b>You have been to this page "+gettimes()+" before.</b>");</script>';
 		echo '<h5>Comments:</h5>'; 
 		echo '<table width="95%">'; 
