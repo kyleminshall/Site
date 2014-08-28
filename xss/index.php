fa55ca4ab215e614101c4aa9c2f3de254910c51d
Kyle Minshall <kyleminshall@gmail.com>
2014-08-27 21:03:46 -0700

    Changing to mysql query

--- xss/index.php
+++ xss/index.php
@@ -1,4 +1,5 @@
 <?php
+
 	ini_set('display_errors',1);
 	error_reporting(E_ALL);
 	
@@ -11,7 +12,7 @@
 	}
 
 	//query comments for this page of this article
-	$query = "SELECT * FROM `comments` WHERE page = '".stripslashes($_SERVER['REQUEST_URI'])."' ORDER BY time ASC";
+	$query = "SELECT * FROM comments WHERE page = '".stripslashes($_SERVER['REQUEST_URI'])."' ORDER BY time ASC";
 
 	$info = mysql_query($query) or trigger_error(mysql_error()." ".$query);
 
@@ -35,75 +36,76 @@
 	  	  	echo '</tr>';
 	 	 }//end while
 		 
-	 	 echo '</table>';
-	  	echo '<hr width="95%" noshade>';
-	  } 
-	  else 
-	  {
-		  echo 'No comments for this page. Feel free to be the first <br>';
-	  }
-	  if(isset($_POST['submit'])) 
-	  {
-		  if(!addslashes($_POST['username'])) die('<u>ERROR:</u> you must enter a username to add a comment.');
-		  if(!addslashes($_POST['contact']))  die('<u>ERROR:</u> enter contact method in contact field.');
-		  if(!addslashes($_POST['subject']))  die('<u>ERROR:</u> enter a subject to your comment.');
-		  if(!addslashes($_POST['comment']))  die('<u>ERROR:</u> cannot add comment if you do not enter one!?');
-		  //this is for a valid contact 
-		
-		  if(substr($_POST['contact'],0,7) != 'mailto:' && !strstr($_POST['contact'],'//')) 
-		  {
-			  if(strstr($_POST['contact'],'@'))
-			  {
-				  $_POST['contact'] = "mailto:".$_POST['contact']."";
-			  }
-			  else
-			  {
-				  $_POST['contact'] = "http://".$_POST['contact']."";
-			  }
-		  } //end valid contact
+		 echo '</table>';
+		 echo '<hr width="95%" noshade>';
+	 } 
+	 else 
+	 {
+		 echo 'No comments for this page. Feel free to be the first <br>';
+	 }
+	 if(isset($_POST['submit'])) 
+	 {
+		 if(!addslashes($_POST['username'])) die('<u>ERROR:</u> you must enter a username to add a comment.');
+		 if(!addslashes($_POST['contact']))  die('<u>ERROR:</u> enter contact method in contact field.');
+		 if(!addslashes($_POST['subject']))  die('<u>ERROR:</u> enter a subject to your comment.');
+		 if(!addslashes($_POST['comment']))  die('<u>ERROR:</u> cannot add comment if you do not enter one!?');
+		 //this is for a valid contact 
 		 
-		  //try to prevent multiple posts and flooding...
-		  $c = "SELECT * from `comments` WHERE ip = '".$_SERVER['REMOTE_ADDR']."'";
+		 if(substr($_POST['contact'],0,7) != 'mailto:' && !strstr($_POST['contact'],'//')) 
+		 {
+			 if(strstr($_POST['contact'],'@'))
+			 {
+				 $_POST['contact'] = "mailto:".$_POST['contact']."";
+			 }
+			 else
+			 {
+				 $_POST['contact'] = "http://".$_POST['contact']."";
+			 }
+		 } //end valid contact
+ 
+		 //try to prevent multiple posts and flooding...
+		 $c = "SELECT * from `comments` WHERE ip = '".$_SERVER['REMOTE_ADDR']."'";
 
-		  $c2 = mysql_query($c);
-		 
-		  while($c3 = mysql_fetch_object($c2)) 
-		  {
-			  $difference = time() - $c3->time;
-			  if($difference < 300) 
-			  {
-				  die('<u>ALERT:</u> '.$c3->username.', You have already commented earlier; if you have a question, try the forums!<BR>');
-			  }
-		  } //end while
-		  
-		  //add comment
-		  $q ="INSERT INTO `comments` (article_id, page, date, time, username, ip, contact, subject, comment) VALUES ('".$_GET['id']."', '".$_POST['page']."', '".$_POST['date']."', '".$_POST['time']."', '".addslashes($_POST['username'])."', '".$_SERVER['REMOTE_ADDR']."', '".addslashes($_POST['contact'])."', '".addslashes($_POST['subject'])."', '".addslashes($_POST['comment'])."')";
+		 $c2 = mysql_query($c);
+ 
+		 while($c3 = mysql_fetch_object($c2)) 
+		 {
+			 $difference = time() - $c3->time;
+			 if($difference < 300) 
+			 {
+				 die('<u>ALERT:</u> '.$c3->username.', You have already commented earlier; if you have a question, try the forums!<BR>');
+			 }
+		 } //end while
+  
+		 //add comment
+		 $q ="INSERT INTO `comments` (article_id, page, date, time, username, ip, contact, subject, comment) VALUES ('".$_GET['id']."', '".$_POST['page']."', '".$_POST['date']."', '".$_POST['time']."', '".addslashes($_POST['username'])."', '".$_SERVER['REMOTE_ADDR']."', '".addslashes($_POST['contact'])."', '".addslashes($_POST['subject'])."', '".addslashes($_POST['comment'])."')";
 
-		  $q2 = mysql_query($q);
-		  
-		  if(!$q2)
-		  {
-			  die(mysql_error());	
-		  } 
-		  //refresh page so they can see new comment
-		  header('Location: http://' . $_SERVER['HTTP_HOST'] . $_POST['page'] . "#commentsrn"); 
-	  } 
-	  else 
-	  {  //display form
+		 $q2 = mysql_query($q);
+  
+		 if(!$q2)
+		 {
+			 die(mysql_error());	
+		 } 
+		 //refresh page so they can see new comment
+		 header('Location: http://' . $_SERVER['HTTP_HOST'] . $_POST['page'] . "#commentsrn"); 
+	 } 
+	 else 
+	 {  //display form
 		  ?>
-			  <form name="comments" action="<? $_SERVER['PHP_SELF']; ?>" method="post">
-			  	  <input type="hidden" name="page" value="<? echo($_SERVER['REQUEST_URI']); ?>">
-				  <input type="hidden" name="date" value="<? echo(date("F j, Y.")); ?>">
-				  <input type="hidden" name="time" value="<? echo(time()); ?>">
-				  <table width="90%" border="0" cellspacing="0" cellpadding="0">
-					  <tr> 
-						  <td><div align="right">Username:   </div></td> 
-						  <td><input name="username" type="text" size="30" value=""></td>
-					  </tr>
-					  <tr> 
-						  <td><div align="right">Contact:   </div></td>
-						  <td><input type="text" name="contact" size="30" value=""> <i>(email or url)</i></td>
-					  </tr>
+		  <form name="comments" action="<? $_SERVER['PHP_SELF']; ?>" method="post">
+			  <input type="hidden" name="page" value="<? echo($_SERVER['REQUEST_URI']); ?>">
+			  <input type="hidden" name="date" value="<? echo(date("F j, Y.")); ?>">
+			  <input type="hidden" name="time" value="<? echo(time()); ?>">
+			  <table width="90%" border="0" cellspacing="0" cellpadding="0">
+				  <tr> 
+					  <td><div align="right">Username:   </div></td> 
+					  <td><input name="username" type="text" size="30" value=""></td>
+				  </tr>
+				  <tr> 
+					  <td><div align="right">Contact:   </div></td>
+					  <td><input type="text" name="contact" size="30" value=""> <i>(email or url)</i></td>
+				  </tr>
+				  <tr>
 					  <td><div align="right">Subject:   </div></td>
 					  <td><input type="text" name="subject" size="30" value=""></td>
 				  </tr>
@@ -114,12 +116,13 @@
 				  <tr> 
 					  <td></td>
 					  <td colspan="2"><input type="reset" value="Reset Fields">     
-						  <input type="submit" name="submit" value="Add Comment"></td>
-					  </tr>
-				  </table>
-			  </form>
-		<?
-	} // end else
+						  <input type="submit" name="submit" value="Add Comment">
+					  </td>
+				  </tr>
+			  </table>
+		  </form>
+		  <?
+	  } // end else
 ?> 
 	  
 		
