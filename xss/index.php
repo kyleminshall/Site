2715ecda5bf81a0a055ceb0d4ec1683985bd4937
Kyle Minshall <kyleminshall@gmail.com>
2014-08-27 22:27:09 -0700

    UGH

--- xss/index.php
+++ xss/index.php
@@ -74,7 +74,7 @@
 		//add comment 
 		$q ="INSERT INTO comments (username, contact, subject, comment)  
 			VALUES ('".addslashes(htmlspecialchars($_POST['username']))."','".addslashes(htmlspecialchars($_POST['contact']))."', 
-		'".addslashes(htmlspecialchars($_POST['subject']))."', '".addslashes(htmlspecialchars(nl2br($_POST['comment'])))."')"; 
+		'".addslashes(htmlspecialchars($_POST['subject']))."', '".addslashes(htmlspecialchars(nl2br($_POST['comment']),false))."')"; 
 
 		$q2 = mysql_query($q) or trigger_error(mysql_error()." ".$q2); 
 		if(!$q2) 
