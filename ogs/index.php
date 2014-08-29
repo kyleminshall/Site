3b332ba1d8cab794c6c445204946f50a9e750b0c
Kyle Minshall <kyleminshall@gmail.com>
2014-08-29 00:53:16 -0700

    Redoing Logout

--- ogs/index.php
+++ ogs/index.php
@@ -52,7 +52,10 @@ membership::confirm();
 				?>
 			</p>
 			<p style="font-size:22px; text-decoration:none">
-				<a style="text-decoration:none" href="logout.php"><button class="turquoise-flat-button">Log Out</button></a>
+				<a style="text-decoration:none" href="comment.php"><button class="turquoise-flat-button">Comments</button></a>
+			</p>
+			<p style="font-size:22px; text-decoration:none">
+				<a style="text-decoration:none" href="logout.php"><button class="turquoise-flat-button" style="color:#FC4144">Log Out</button></a>
 			</p>
 		</div><!-- end main --> 
 	</body>
