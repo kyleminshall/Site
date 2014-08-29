fee76f16e4818693b347c05e916c1d8a48e30b61
Kyle Minshall <kyleminshall@gmail.com>
2014-08-28 22:53:19 -0700

    Adding sign up ability

--- ogs/classes/membership.php
+++ ogs/classes/membership.php
@@ -18,6 +18,29 @@ class Membership
 			return $ensure_credentials;
 		}
 	}
+	
+	function logOut()
+	{
+		if(isset($_SESSION['status']))
+		{
+			unset($_SESSION['status']);
+			
+			if(isset($_COOKIE[session_name()]))
+			{
+				setcookie(session_name(), '', time() - 10000);
+			}
+			session_destroy();
+		}
+	}
+	
+	function confirm()
+	{
+		session_start();
+		if($_SESSION['status'] != 'authorized')
+		{
+			header("location: login.php");
+		}
+	}
 }
 
 ?>
