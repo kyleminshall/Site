599b1a05d18d976ea9d0dd9c2c01fd47a42b892a
Kyle Minshall <kyleminshall@gmail.com>
2014-08-28 20:58:56 -0700

    Adding signin page

--- ogs/signup.php
+++ ogs/signup.php
@@ -12,19 +12,16 @@
 		<div id="login">
 			<div id="box">
 				<p>
-					Sign Up!
+					<h4>Sign Up!</h4>
 				</p>
 				<form method="post" action="">
-					<a href="signup.php">
-						<img class="profile-img" src="css/avatar_2x.png" alt="">
-					</a>
 					<p>
 						<input class="form" id="number" name="number" type="text" placeholder="Permission Key" ><br>
 						<input class="form" id="Password" name="firstName" type="text" placeholder="First Name" ><br>
 						<input class="form" id="Password" name="lastName" type="text" placeholder="Last Name" ><br>
 						<input class="form" id="Password" name="password" type="password" placeholder="Password"><br>
 						<input class="form" id="Password" name="password" type="password" placeholder="Confirm Password"><br>
-						<button type="submit" name="submit" class="btn btn-4 btn-4a icon-arrow-right">Sign Up</button>
+						<button type="submit" name="submit" class="btn btn-4 btn-4a icon-arrow-right" style="padding:10px 62px !important;">Sign Up!</button>
 					</p>
 				</form>
 			</div>
