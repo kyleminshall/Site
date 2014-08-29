f973a300f5ceda0ca88e40484c32a6d9ea5e62f3
Kyle Minshall <kyleminshall@gmail.com>
2014-08-29 02:19:04 -0700

    Adding features

--- ogs/logout.php
+++ ogs/logout.php
@@ -1,13 +1,11 @@
 <?php
-
-
 	$con=mysql_connect("localhost","KyleM","Minshall1!");
 	$db_selected = mysql_select_db('Site', $con);
 	
 	$date = date("Y-m-d H:i:s");
 	$username = $_SESSION['username'];
 	
-	$q ="INSERT INTO OGs (last_login) VALUES ('$date') WHERE username='$username'"; 
+	$q ="UPDATE OGs SET last_login='$date' WHERE username='$username'"; 
 	$q2 = mysql_query($q) or trigger_error(mysql_error()." ".$q); 
 	
 	header("location: login.php");
