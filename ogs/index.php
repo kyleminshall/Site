89a929a6413ec0f03d49a0db42235fbad127eb67
Kyle Minshall <kyleminshall@gmail.com>
2014-08-28 23:31:32 -0700

    Adding sign up ability

--- ogs/index.php
+++ ogs/index.php
@@ -19,6 +19,14 @@ $membership->confirm();
 		<link rel="stylesheet" type="text/css" href="css/default.css">
 	</head>
 	<body style="background-image: none;">
+		<div id="top">
+			<center>
+				<p>
+					Website version 0.0.1<br>
+					The OG Social Network
+				</p>
+			</center>
+		</div>
 		<div id="main">
 			<p>
 				<?php 
