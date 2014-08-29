86c5e5d2b8ca5a07905acc0c8d47b832dc1388de
Kyle Minshall <kyleminshall@gmail.com>
2014-08-29 00:22:28 -0700

    Redoing Logout

--- ogs/logout.php
+++ ogs/logout.php
@@ -4,11 +4,16 @@
 
 	if(isset($_SESSION))
 	{
-		session_destroy();
+		$_SESSION = array();
 		
-		if(isset($_COOKIE[session_name()]))
-		{
-			setcookie('PHPSESSID', '', time() - 10000);
+		if (ini_get("session.use_cookies")) {
+		    $params = session_get_cookie_params();
+		    setcookie(session_name(), '', time() - 42000,
+		        $params["path"], $params["domain"],
+		        $params["secure"], $params["httponly"]
+		    );
 		}
 	}
+	
+	session_destroy();
 ?>
