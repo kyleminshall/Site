266c6821ea9df84f90c3577069bdbd320e058fdd
Kyle Minshall <kyleminshall@gmail.com>
2014-08-28 20:44:06 -0700

    Fixing login page

--- ogs/index.php
+++ ogs/index.php
@@ -25,7 +25,7 @@ $membership->confirm();
 				
 					$username = $_GET['user'];
 					$name_row = mysql_fetch_assoc(mysql_query("SELECT name FROM OGs WHERE username='$username'"));
-					$name = $row['name'];
+					$name = $name_row['name'];
 					$admin_row = mysql_fetch_assoc(mysql_query("SELECT is_admin as admin FROM OGs WHERE username='$username'"));
 					$is_admin = $admin_row['admin']==1;
 				
