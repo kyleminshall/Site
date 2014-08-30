36f99a26ab1a04cfb9c23cb2f5ac03af20ab411f
Kyle Minshall <kyleminshall@gmail.com>
2014-08-30 09:55:19 -0700

    Adding features

--- ogs/index.php
+++ ogs/index.php
@@ -47,6 +47,9 @@ membership::confirm();
 				<a style="text-decoration:none" href="comments.php"><button class="turquoise-flat-button">Comments</button></a>
 			</p>
 			<p style="font-size:22px; text-decoration:none">
+				<a style="text-decoration:none" href="progress.php"><button class="turquoise-flat-button">Work in Progress</button></a>
+			</p>
+			<p style="font-size:22px; text-decoration:none">
 				<a style="text-decoration:none" href="logout.php"><button class="turquoise-flat-button" style="background:#FC4144">Log Out</button></a>
 			</p>
 		</div><!-- end main --> 
