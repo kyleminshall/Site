4752d8df9a98dfcf3e270d80d6fb95d0e0782e9c
Kyle Minshall <kyleminshall@gmail.com>
2014-08-29 17:02:54 -0700

    Adding features

--- ogs/comments.php
+++ ogs/comments.php
@@ -134,9 +134,11 @@
 		$rep = "SELECT * FROM replies WHERE post='$info2->id'";
 		$replies = mysql_query($rep) or trigger_error(mysql_error())." ".$rep;
 		while($replies2 = mysql_fetch_object($replies)) 
+		{
   		echo '<tr>'; 
   		echo '<td colspan="3"> <p style="font-size:18px;color:000">'.stripslashes($replies2->reply).'</p><br></td>'; 
   		echo '</tr>';
+		}	
 		echo '</tr>';
 		echo '</table>';
 		echo '<br><br>';
