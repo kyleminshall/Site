17c29a267b9b975ce47c7f3688868fc32d10d44d
Kyle Minshall <kyleminshall@gmail.com>
2014-08-30 15:29:13 -0700

    Adding JQuery

--- ogs/js/like.js
+++ ogs/js/like.js
@@ -2,12 +2,7 @@ function like_add(post_id){
 	$.post('classes/like_add.php', {post_id:post_id}, function(data) {
 		if(data == "success") {
 			like_get(post_id);
-		    if ($('#like_'+$post_id).text() == 'Like') {
-		        $('#like_'+$post_id).text('Unlike');
-		    }
-		    else {
-		        $('#like_'+$post_id).text('Like');
-		    }
+			like_update(post_id);
 		} else {
 			alert(data);
 		}
@@ -17,5 +12,16 @@ function like_add(post_id){
 function like_get(post_id) {
 	$.post('classes/like_get.php', {post_id:post_id}, function(data) {
 		$('#post_'+post_id+'_likes').text(data);
+	});
+}
+
+function like_update(post_id) {
+	$.post('', {post_id:post_id}, function(data) {
+		if ($('#like_'+$post_id).text() == 'Like') {
+		    $('#like_'+$post_id).text('Unlike');
+		}
+		else {
+		    $('#like_'+$post_id).text('Like');
+		}
 	});
 }
