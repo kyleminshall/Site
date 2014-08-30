8ce6649d21bacead8be2407a783d9c635c83cbcf
Kyle Minshall <kyleminshall@gmail.com>
2014-08-29 17:21:48 -0700

    Adding features

--- ogs/comments.php
+++ ogs/comments.php
@@ -128,9 +128,16 @@
 		echo '</tr>';
 		while($replies2 = mysql_fetch_object($replies)) 
 		{
-  		echo '<tr style="background-color:#f6f6f6;">'; 
-  		echo '<td colspan="3"> <p style="font-size:14px;color:000;margin:0"><b>'.stripslashes($replies2->username).'</b> : '.stripslashes($replies2->reply).'</p></td>'; 
-  		echo '</tr>';
+			$time = strtotime($replies2->date);
+			$replied = date("m/d/y \a\\t g:i A", $time);
+  			echo '<tr style="background-color:#f6f6f6;">'; 
+  			echo '<td colspan="3"> 
+					<p style="font-size:14px;color:000;margin:0">
+						<b>'.stripslashes($replies2->username).'</b> : '.stripslashes($replies2->reply).'<br>
+						<span style="font-size:12px;color:#494949;">'.$replied.'</span>
+					</p>
+				  </td>'; 
+  			echo '</tr>';
 		}
 		echo '<tr style="background-color:#f6f6f6;border-top:1px solid black;">';
 		echo '<td colspan="3" style="padding:5px">
