83522f61024d78ff315f1a2b7ea35db310ce6caf
Kyle Minshall <kyleminshall@gmail.com>
2014-08-29 17:09:29 -0700

    Adding features

--- ogs/comments.php
+++ ogs/comments.php
@@ -127,7 +127,7 @@
 		$replies = mysql_query($rep) or trigger_error(mysql_error())." ".$rep;
 		while($replies2 = mysql_fetch_object($replies)) 
 		{
-  		echo '<tr>'; 
+  		echo '<tr style="background-color:#f6f6f6;">'; 
   		echo '<td colspan="3"> <p style="font-size:14px;color:000;margin:0">'.stripslashes($replies2->username).' : '.stripslashes($replies2->reply).'</p></td>'; 
   		echo '</tr>';
 		}
