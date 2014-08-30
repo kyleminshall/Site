833a8722872801e6ede4067597c573f22954810a
Kyle Minshall <kyleminshall@gmail.com>
2014-08-30 15:34:11 -0700

    Adding JQuery

--- ogs/comments.php
+++ ogs/comments.php
@@ -109,21 +109,8 @@
 		echo '<tr>'; 
 		echo '<td colspan="3" style="padding-left: 10px;">
 				<p style="font-size:12px;padding:0;text-align:left">
-					<a style="text-decoration:none;color:#1F80C9;" href="#" onclick="like_add('.$post_number.');return false;">
+					<a style="text-decoration:none;color:#1F80C9;" href="#">
 						<span class="like">Like</span>
-						<script>
-						function LikeUnLikeButton() {
-						            $(".like").click(function (e) {
-						                if ($(this).html() == "Like") {
-						                    $(this).html("Unlike");
-						                }
-						                else {
-						                    $(this).html("Like");
-						                }
-						            });
-						        }
-						LikeUnLikeButton();
-						</script>
 					</a>
 				</p>
 			  </td>'; 
