693ba1c8e4b0e84b5164050882e90df91e9c8b94
Kyle Minshall <kyleminshall@gmail.com>
2014-09-01 09:30:29 -0700

    Adding JQuery

--- ogs/js/like.js
+++ ogs/js/like.js
@@ -26,6 +26,5 @@ function changeText(idElement) {
 
 function initialize(idElement)
 {
-	document.getElementById('like_'+idElement).innerHTML= (wch ? 'Like' : 'Unlike');
-	alert(document.getElementById('like_'+idElement).innerHTML= (wch ? 'Like' : 'Unlike'));
+	document.getElementById('like_'+idElement).innerHTML= 'Like';
 }
