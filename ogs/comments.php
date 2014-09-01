f8bb29a9a3606f642492eab169f12d461b090d5e
Kyle Minshall <kyleminshall@gmail.com>
2014-08-31 23:23:46 -0700

    Adding JQuery

--- ogs/comments.php
+++ ogs/comments.php
@@ -111,7 +111,7 @@
 		echo '<tr>'; 
 		echo '<td colspan="3" style="padding-left: 10px;">
 				<p style="font-size:12px;padding:0;text-align:left">
-					<a class="like" id="like_'.$post_number.'" style="text-decoration:none;color:#1F80C9;" href="#" onclick="like_add('.$post_number.');changeText('.$post_number.');return false;">
+					<a class="like" id="like_'.$post_number.'" style="text-decoration:none;color:#1F80C9;" href="#" onclick="like_add('.$post_number.');return false;">
 						'.$button.'
 					</a>
 				</p>
--- ogs/js/like.js
+++ ogs/js/like.js
@@ -1,6 +1,7 @@
 function like_add(post_id){
 	$.post('classes/like_add.php', {post_id:post_id}, function(data) {
 		like_get(post_id);
+		changeText(post_id);
 	});
 }
 
@@ -8,4 +9,16 @@ function like_get(post_id) {
 	$.post('classes/like_get.php', {post_id:post_id}, function(data) {
 		$('#post_'+post_id+'_likes').text(data);
 	});
+}
+
+function changeText(idElement) {
+    var element = document.getElementById('like_'+idElement);
+    if (element.innerHTML === 'Like')
+	{
+    	element.innerHTML = 'Unlike';
+    } 
+    else 
+	{
+        element.innerHTML = 'Like';
+    }
 }
