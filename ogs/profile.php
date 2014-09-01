aa0aa62b484882b1b940cfe91bc81dae4517a057
Kyle Minshall <kyleminshall@gmail.com>
2014-09-01 11:21:35 -0700

    Adding Profile code

---
+++ ogs/profile.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en" class="no-js">
+	<head>
+		<meta charset="UTF-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> 
+		<title>Profile</title>
+		<link rel="stylesheet" type="text/css" href="css/default.css">
+	</head>
+	<body style="background-image: none;">
+		<div id="main">
+			<p>
+				Current profile picture:
+			</p>
+		</div><!-- end main --> 
+	</body>
+</html>
