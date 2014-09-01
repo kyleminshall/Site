90004752948ccacc93a7a7421664b173470dac07
Kyle Minshall <kyleminshall@gmail.com>
2014-09-01 09:15:31 -0700

    Adding JQuery

--- ogs/comments.php
+++ ogs/comments.php
@@ -100,7 +100,7 @@
 		$count = mysql_num_rows($replies);
 		$likes = mysql_result(mysql_query("SELECT likes FROM posts WHERE id='$post_number'"),0);
 		$unlike = (mysql_result(mysql_query("SELECT COUNT(id) FROM likes WHERE username='$username' AND post='$post_number'") , 0) == 0) ? false : true;
-		$button = $unlike == true ? 'Unlike' : 'Like';
+		$button = $unlike == true ? utf8_encode('Unlike') : utf8_encode('Like');
 		echo '<td style="width:65%"><p style="font-size:18px;color:000"><b>'.stripslashes($info2->username).'</b><br><span style="font-size:12px;color:#494949;">'.$submitted.'</span></p></td>'; 
 		echo '<td style="width:30%;padding:0;"><p style="font-size:14px;color:000;text-align:right">Likes :<br>Comments :</p></td>'; 
 		echo '<td style="width:5%;padding:0;"><p style="font-size:14px;color:000;text-align:center"><span id="post_'.$post_number.'_likes">'.$likes.'</span><br>'.$count.' </p></td>';
@@ -112,7 +112,7 @@
 		echo '<td colspan="3" style="padding-left: 10px;">
 				<p style="font-size:12px;padding:0;text-align:left">
 					<a id="like_'.$post_number.'" style="text-decoration:none;color:#1F80C9;" href="#" onclick="like_add('.$post_number.');return false;">
-						Like
+						'.$button.'
 					</a>
 				</p>
 			  </td>'; 
