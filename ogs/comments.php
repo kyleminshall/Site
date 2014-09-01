b74d8859b574864510c008b4cdfd0284f1fa7316
Kyle Minshall <kyleminshall@gmail.com>
2014-08-31 23:15:17 -0700

    Adding JQuery

--- ogs/comments.php
+++ ogs/comments.php
@@ -112,7 +112,7 @@
 		echo '<tr>'; 
 		echo '<td colspan="3" style="padding-left: 10px;">
 				<p style="font-size:12px;padding:0;text-align:left">
-					<a class="like" id="'.$count.'" style="text-decoration:none;color:#1F80C9;" href="#" onclick="like_add('.$post_number.');changeText(1);return false;">
+					<a class="like" id="'.$count.'" style="text-decoration:none;color:#1F80C9;" href="#" onclick="like_add('.$post_number.');changeText('.$count.');return false;">
 						'.$button.'
 					</a>
 				</p>
