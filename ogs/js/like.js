49e4d2288e8c06941f5b0e3d8397a2552237b882
Kyle Minshall <kyleminshall@gmail.com>
2014-08-31 22:56:21 -0700

    Adding JQuery

--- ogs/js/like.js
+++ ogs/js/like.js
@@ -9,3 +9,13 @@ function like_get(post_id) {
 		$('#post_'+post_id+'_likes').text(data);
 	});
 }
+
+$(".like").click(function(e) {
+    if ($(this).html() == "Like") {
+        $(this).html('Unlike');
+    }
+    else {
+        $(this).html('Like');
+    }
+    return false;
+});​
