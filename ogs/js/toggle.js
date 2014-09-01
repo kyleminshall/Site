8462ac06deed37056a27aa88ba7b518206c4c369
Kyle Minshall <kyleminshall@gmail.com>
2014-08-31 22:58:18 -0700

    Adding JQuery

---
+++ ogs/js/toggle.js
@@ -0,0 +1,9 @@
+$(".like").click(function(e) {
+    if ($(this).html() == "Like") {
+        $(this).html('Unlike');
+    }
+    else {
+        $(this).html('Like');
+    }
+    return false;
+});​
