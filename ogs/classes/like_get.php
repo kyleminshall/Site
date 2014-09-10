76ce1a86fe90fbfacd4008eff0caec9ed7fcf62c
Kyle Minshall <kyleminshall@gmail.com>
2014-09-10 06:26:24 +0000

    Removing OGs site

--- ogs/classes/like_get.php
+++
@@ -1,8 +0,0 @@
-<?php
-	include 'like.php';
-	
-	if(isset($_POST['post_id'], $_SESSION['username']) && exists($_POST['post_id']))
-	{
-		echo like_count($_POST['post_id']);
-	}
-?>
--- ogs/js/expand.js
+++
@@ -1,6 +0,0 @@
-$(document).on('input.textarea', '.autoExpand', function(){
-    var minRows = this.getAttribute('data-min-rows')|0,
-        rows    = this.value.split("\n").length;
-
-    this.rows = rows < minRows ? minRows : rows;
-});
