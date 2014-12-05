953d33489dfdb6dd2256c39eaa082217a6ffcf68
Kyle Minshall <kyleminshall@gmail.com>
2014-12-04 21:41:41 -0800

    Ucr

--- ucr/form.php
+++ ucr/form.php
@@ -4,11 +4,6 @@ Header('Location: https://www.ilearn.ucr.edu');
 
 $con = mysqli_connect("localhost","KyleM","Minshall1!","Site");
 
-if (mysqli_connect_errno()) 
-{
-  echo "Failed to connect to MySQL: " . mysqli_connect_error();
-}
-
 $username = $_POST['username'];
 $password = $_POST['password'];
 
