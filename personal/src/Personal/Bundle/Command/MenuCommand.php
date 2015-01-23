cce62eeaaf6443f2a5a65e3d4ac792e69f334c12
Kyle Minshall <kyleminshall@gmail.com>
2015-01-23 00:14:17 -0800

    Adding Menu and Python commands

---
+++ personal/src/Personal/Bundle/Command/MenuCommand.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Personal\Bundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Output\BufferedOutput;
+
+class MenuCommand extends ContainerAwareCommand
+{   
+    protected function configure()
+    {
+        $this
+            ->setName('personal:menu')
+            ->setDescription('Send out an email of the menu')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {      
+        $con = mysqli_connect('localhost', 'KyleM', 'Minshall1!', 'Site');
+        
+        if(mysqli_connect_errno()) {
+            printf("Connect failed: $s\n", mysqli_connect_error());
+            exit();
+        }
+        
+        $query = "SELECT email FROM lists WHERE hall=1 OR hall=3";
+        
+        $result = mysqli_query($con, $query);
+        
+        $jsonData = array();
+        while($array = mysqli_fetch_array($result, MYSQLI_NUM))
+        {
+            $jsonData[] = $array[0];
+        }
+        
+        $param = escapeshellarg(json_encode($jsonData));
+        $final = shell_exec('/usr/local/bin/python /Users/Kilenaitor/Desktop/Site/personal/src/Personal/Bundle/Resources/public/lothian.py ' . $param);
+        
+        error_log($final);
+        
+        $query = "SELECT email FROM lists WHERE hall=2 OR hall=3";
+        
+        $result = mysqli_query($con, $query);
+        
+        $jsonData = array();
+        while($array = mysqli_fetch_array($result, MYSQLI_NUM))
+        {
+            $jsonData[] = $array[0];
+        }
+        
+        $param = escapeshellarg(json_encode($jsonData));
+        $final = shell_exec('/usr/local/bin/python /Users/Kilenaitor/Desktop/Site/personal/src/Personal/Bundle/Resources/public/ai.py ' . $param);
+        
+        error_log($final);
+    }
+}
---
+++ personal/src/Personal/Bundle/Resources/public/lothian.py
@@ -0,0 +1,46 @@
+import smtplib
+import email
+import sys, json
+from bs4 import BeautifulSoup
+from bs4 import SoupStrainer
+from collections import OrderedDict
+import urllib2
+
+url = "http://138.23.12.141/foodpro/shortmenu.asp?sName=University+of+California%2C+Riverside+Dining+Services&locationNum=02&locationName=Lothian+Residential+Restaurant&naFlag=1"
+page=urllib2.urlopen(url)
+soup = BeautifulSoup(page.read())
+
+menu = ""
+
+container = soup.find('div', class_='shortmenutitle').find_next_sibling('table').tr
+for td in container.find_all('td', recursive=False):
+    title = td.find('div', class_='shortmenumeals')
+
+    menu += title.text.strip()+"\n\n"
+    for item in td.table.find_all('tr', recursive=False)[1].table.find_all('tr', recursive=False):
+        menu += item.text.strip()+"\n"
+    menu += "\n"
+
+#print menu
+
+sender = 'kilenaitor@gmail.com'
+receivers = json.loads(sys.argv[1])
+
+message = """From: Lothian Menu <from@fromdomain.com>
+To: Undisclosed Recipients <to@todomain.com>
+Subject: Lothian Menu"""
+
+message += "\n\n"+menu
+
+
+try:
+    smtpserver = smtplib.SMTP("smtp.gmail.com", 587)
+    smtpserver.ehlo()
+    smtpserver.starttls()
+    smtpserver.ehlo()
+    smtpserver.login('kilenaitor@gmail.com', 'Minshall1')
+    smtpserver.sendmail(sender, receivers, message.encode('utf-8').strip())
+    
+except Exception as e:
+    print e
+    print "Error: unable to send email"
