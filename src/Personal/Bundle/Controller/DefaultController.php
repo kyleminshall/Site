a6e24e6d5e88a32bc430af14baad82eee56218dc
Kyle Minshall <kyleminshall@gmail.com>
2016-04-07 22:41:18 -0700

    Moving erros to top in case I'm crazy

--- src/Personal/Bundle/Controller/DefaultController.php
+++ src/Personal/Bundle/Controller/DefaultController.php
@@ -1,14 +1,13 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 namespace Personal\Bundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 require_once '../../../../vendor/autoload.php';
-
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
 
 include('database.php');
 include('secrets.php');
