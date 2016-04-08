84f772228f4c2d9616b794f69a7548f551996f1a
Kyle Minshall <kyleminshall@gmail.com>
2016-04-07 22:35:11 -0700

    Need to add link to autoload

--- src/Personal/Bundle/Controller/DefaultController.php
+++ src/Personal/Bundle/Controller/DefaultController.php
@@ -4,6 +4,8 @@ namespace Personal\Bundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
+require_once '../../../../vendor/autoload.php';
+
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
