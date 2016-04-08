4d775dbdabefaa5834e63d5f6231df7772b7e03d
Kyle Minshall <kyleminshall@gmail.com>
2016-04-07 22:57:02 -0700

    Moving erros to top in case I'm crazy

--- src/Personal/Bundle/Controller/DefaultController.php
+++ src/Personal/Bundle/Controller/DefaultController.php
@@ -7,7 +7,7 @@ error_reporting(E_ALL);
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-require_once __DIR__.'../../../../vendor/autoload.php';
+require_once __DIR__.'/../../../../vendor/autoload.php';
 
 include('database.php');
 include('secrets.php');
