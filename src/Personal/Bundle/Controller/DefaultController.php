ebedc12c55d4dfde7cadc56bf608f7add2bfbb5c
Kyle Minshall <kyleminshall@gmail.com>
2016-04-07 21:16:38 -0700

    Fixing errors

--- src/Personal/Bundle/Controller/DefaultController.php
+++ src/Personal/Bundle/Controller/DefaultController.php
@@ -5,6 +5,9 @@ namespace Personal\Bundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 include('database.php');
+include('secrets.php');
+
+$accessToken = "";
 
 class DefaultController extends Controller
 {
@@ -111,4 +114,98 @@ class DefaultController extends Controller
     {
         return $this->render('PersonalBundle:Default:shitpost.html.twig');
     }
+    
+    public function lothianAction()
+    {   
+        if(isset($_POST) && isset($_POST['command']) && isset($_POST['token'])) {
+            if($token !== "kytAx7z4AnLpKEzZZSMSMQoY"){ 
+                $msg = "The token for the slash command doesn't match. Check your script.";
+                die($msg);
+                echo $msg;
+            }
+            
+            $client = new Google_Client();
+            $service = new Google_Service_Calendar($client);
+            
+            $default_start_string = "Y-m-d\T17:00:00-07:00";    // 5 PM
+            $default_end_string = "Y-m-d\T18:00:00-07:00";      // 6 PM
+            $command = $_POST['command'];
+            
+            $date_given = isset($_POST['text']);
+            
+            $start_time = isset($_POST['text']) 
+                    ? date($default_start_string, strtotime($_POST['text'])) 
+                    : date($default_start_string);
+            $end_time = isset($_POST['text'])
+                    ? date($default_end_string, strtotime($_POST['text'])) 
+                    : date($default_end_string);
+            
+            $calendarId = CALENDAR_ID;
+            $optParams = array(
+              'maxResults' => 1,
+              'orderBy' => 'startTime',
+              'singleEvents' => TRUE,
+              'timeMin' => $start_time,
+              'timeMax' => $end_time
+            );
+            $results = $service->events->listEvents($calendarId, $optParams);
+
+            if (count($results->getItems()) === 0) {
+              print "Nothing found for that day.\n";
+            } else {
+              foreach ($results->getItems() as $event) {
+                  $duty = $event->summary;
+                  printf("*%s* is on duty.", $duty);
+              }
+            }
+        }
+    }
+    
+    /**
+     * Returns an authorized API client.
+     * @return Google_Client the authorized client object
+     */
+    function getClient() {
+        $client = new Google_Client();
+        $client->setApplicationName('Slack Google Calendar');
+        $client->setScopes('Google_Service_Calendar::CALENDAR_READONLY');
+        $client->setAuthConfigFile(CLIENT_SECRET_PATH);
+        $client->setAccessType('offline');
+
+        $accessToken = getAccessToken();
+        $client->setAccessToken($accessToken);
+
+        // Refresh the token if it's expired.
+        if ($client->isAccessTokenExpired()) {
+            $client->refreshToken($client->getRefreshToken());
+            $accessToken = $client->getAccessToken();
+        }
+        return $client;
+    }
+    
+    /**
+     * Function to get access token from Google OAuth
+     * @return Google API Access Token
+     */
+    private function getAccessToken() 
+    {
+        $tokenURL = 'https://accounts.google.com/o/oauth2/token';
+        $postData = array(
+            'client_secret'=> CLIENT_SECRET,
+            'grant_type'=> 'refresh_token',
+            'refresh_token'=> REFRESH_TOKEN,
+            'client_id'=> CLIENT_ID
+        );
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $tokenURL);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $postData);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+        $tokenReturn = curl_exec($ch);
+        $token = json_decode($tokenReturn);
+        $accessToken = $token->access_token;
+        return $accessToken;
+    }
+    
 }
